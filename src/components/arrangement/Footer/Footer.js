a03929e6eb17e47b00cb817d8fde682b192344c8
Tsukichan555 <Tsukichan555.dev@gmail.com>
2024-12-31 00:18:50 +0900

    gitignoreに.nextを追加。FooterのMockを追加。サイト共通の定数を定義するconstを追加。

--- src/components/arrangement/Footer/Footer.js
+++ src/components/arrangement/Footer/Footer.js
@@ -1,9 +1,21 @@
-import React from 'react'
-import styles from './Footer.module.css'
+import React from 'react';
+import styles from './Footer.module.css';
+import siteData from '../../../const';
 
 const Footer = () => {
+  const instagramUrl = siteData.link.social.instagram;
+  const xUrl = siteData.link.social.x;
+
   return (
-    <div className={styles.Footer}>Footer</div>
+    <div className={styles.Footer}>
+      Footer
+      <div>
+        <a href={instagramUrl} target="_blank" rel="noopener noreferrer">Instagram</a>
+      </div>
+      <div>
+        <a href={xUrl} target="_blank" rel="noopener noreferrer">X</a>
+      </div>
+    </div>
   )
 }
 
---
+++ src/const.js
@@ -0,0 +1,20 @@
+const siteData = {
+	title: "meltomir",
+    desc: "meltomirのポートフォリオサイトです。アート作品の販売は外部リンクをご参照ください。",
+    url: "",
+    lang: "ja",
+    link: {
+        social: {
+            instagram: "https://www.instagram.com/meltomir/",
+            x: "https://x.com/nemr_oz/",
+        },
+        shop: {
+            base: "",
+            booth: "",
+        },
+        writing: {
+            note: "",
+        },
+    },
+};
+export default siteData;
